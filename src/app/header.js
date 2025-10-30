c2e152efbb1f3cedc64c002d3f59f8d59ab88cca
if-haikal <fasalalifhaikalirawan@gmail.com>
2025-10-30 23:52:47 +0700

    feat: Refactor Back again

--- src/app/header.js
+++ src/app/header.js
@@ -1,20 +1,19 @@
 import Link from "next/link";
 import { getSetting } from "./queries/getSetting";
-import Navbar from "./nav";
-import { Suspense } from "react";
 
-export function Header(){
-    const setting = getSetting();
+export async function Header(){
+    const setting = await getSetting();
 
     return (
         <header>
 
-            {/* <h1 className="text-3xl font-bold mt-2 mx-2">{setting.sitename}</h1> */}
-            
-            <Suspense fallback={<div className="mx-2 text-red-400">Loading...</div>}>
-                <Navbar props={setting} />
-            </Suspense>
-            
+            <h1 className="text-3xl font-bold mt-2 mx-2">{setting.sitename}</h1>
+
+            <nav className="mx-2 flex gap-4 border-b-2 pb-2 mb-4">
+                <Link href="/">Home</Link>
+                <Link href="/articles">Articles</Link>
+                <Link href="/products">Product</Link>
+            </nav>
         
         </header>
     )
--- src/app/queries/getSetting.js
+++ src/app/queries/getSetting.js
@@ -1,11 +1,4 @@
 export async function getSetting() {
-    
-    await new Promise((resolve) =>
-        setTimeout(() => {
-            resolve(true);
-        }, 3000)
-    );
-
     const res = await fetch("http://localhost:3001/setting");
     const data = await res.json();
 
