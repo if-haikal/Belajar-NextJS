dfbdc21f4fdddc1f0f69dd4321188cf73bf3fa98
if-haikal <fasalalifhaikalirawan@gmail.com>
2025-11-01 21:20:09 +0700

    feat: Add Request Body from API

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -9,8 +9,20 @@ export async function GET(req){
 }
 
 
-export async function POST() {
+export async function POST(req) {
+    const body = await req.json();
+
+    // const formData = await req.formData();
+    // const name = formData.get('name');
+    // const email = formData.get('email');
+
+    console.log(body);
+
     return Response.json({
-        message: "User POST Response from static route"
+        user : {
+            name: body.name,
+            email: body.email,
+            username: body.username
+        }
     });
 }
